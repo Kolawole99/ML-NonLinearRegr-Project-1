e039da56ae2de16e167237483e157b5faaf11c3b
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 10:46:06 +0100

    Choosing a model

--- NLR.py
+++ NLR.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #%matplotlib inline #needed in jupyter 
 import pandas as pd
+from scipy.optimize import curve_fit
 
 
 
@@ -19,6 +20,49 @@ plt.plot(x_data, y_data, 'ro')
 plt.ylabel('GDP')
 plt.xlabel('Year')
 plt.show()
+
+
+
+#======================================CHOOSING A MODEL===================================
+#We choose a sigmoid or logistic curve to train the model because of its shape.
+def sigmoid(x, Beta_1, Beta_2):
+     y = 1 / (1 + np.exp(-Beta_1*(x-Beta_2)))
+     return y
+
+#Lets look at a sample sigmoid line that might fit with the data:
+beta_1 = 0.10
+beta_2 = 1990.0
+#logistic function
+Y_pred = sigmoid(x_data, beta_1 , beta_2)
+#plot initial prediction against datapoints
+plt.plot(x_data, Y_pred*15000000000000.)
+plt.plot(x_data, y_data, 'ro')
+# Lets normalize our data to be able to fid the best parameters to fit the data with
+xdata =x_data/max(x_data)
+ydata =y_data/max(y_data)
+#use curve_fit to find the best line thast fits the data well
+popt, pcov = curve_fit(sigmoid, xdata, ydata)
+#print the final parameters
+print(" beta_1 = %f, beta_2 = %f" % (popt[0], popt[1]))
+#we plot our optimized data and fitted line
+x = np.linspace(1960, 2015, 55)
+x = x/max(x)
+plt.figure(figsize=(8,5))
+y = sigmoid(x, *popt)
+plt.plot(xdata, ydata, 'ro', label='data')
+plt.plot(x,y, linewidth=3.0, label='fit')
+plt.legend(loc='best')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
+
+
+
+
+
+
+
+
 
 
 
@@ -77,3 +121,14 @@ plt.show()
 # plt.ylabel('Dependent Variable')
 # plt.xlabel('Indepdendent Variable')
 # plt.show()
+
+"""
+Pure plot of the Logistic function
+    X = np.arange(-5.0, 5.0, 0.1)
+    Y = 1.0 / (1.0 + np.exp(-X))
+
+    plt.plot(X,Y) 
+    plt.ylabel('Dependent Variable')
+    plt.xlabel('Indepdendent Variable')
+    plt.show()
+"""
